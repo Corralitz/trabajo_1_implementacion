272200f13299d79205f85c6f74cf868bb1439002
corralitz <sacosa634@gmail.com>
2025-08-06 17:14:13 -0500

    DAO Bully corregido

--- src/main/java/com/example/bullying/dao/IBullyDAO.java
+++ src/main/java/com/example/bullying/dao/IBullyDAO.java
@@ -2,7 +2,8 @@ package com.example.bullying.dao;
 
 import com.example.bullying.models.Bully;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface IBullyDAO extends JpaRepository<Bully, Long> {
-    Optional
+    Optional<Bully> findBullyByName(String name);
 }
