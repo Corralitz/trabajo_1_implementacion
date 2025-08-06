ba7a3c9653c61cc6e2be4fa9dbed0754d8d7d7fc
corralitz <sacosa634@gmail.com>
2025-08-06 17:10:38 -0500

    DAOs de Bully, Clique y Media

--- src/main/java/com/example/bullying/dao/IBullyDAO.java
+++ src/main/java/com/example/bullying/dao/IBullyDAO.java
@@ -1,4 +1,8 @@
 package com.example.bullying.dao;
 
-public interface IBullyDAO {
+import com.example.bullying.models.Bully;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IBullyDAO extends JpaRepository<Bully, Long> {
+    Optional
 }
