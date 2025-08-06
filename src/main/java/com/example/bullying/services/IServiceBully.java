89880966d2c76d5a9975b56cc36e80247085d48d
corralitz <sacosa634@gmail.com>
2025-08-06 18:19:59 -0500

    Service Bullies

--- src/main/java/com/example/bullying/services/IServiceBully.java
+++ src/main/java/com/example/bullying/services/IServiceBully.java
@@ -1,4 +1,15 @@
 package com.example.bullying.services;
 
+import com.example.bullying.dto.BullyDTO;
+import com.example.bullying.models.Bully;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IServiceBully {
+    public List<BullyDTO> getBullies();
+    public Bully addBully(BullyDTO bully);
+    public Optional<Bully> removeBully(Long id);
+    public Optional<Bully> getBullyById(Long id);
+    public Optional<Bully> getBullyByName(String name);
 }
