aa7c6eae769aeffaf5b23c490f36f3b19e6da3a0
corralitz <sacosa634@gmail.com>
2025-08-06 16:51:20 -0500

    Corregido el modelo de Media

--- src/main/java/com/example/bullying/models/Media.java
+++ src/main/java/com/example/bullying/models/Media.java
@@ -1,10 +1,19 @@
 package com.example.bullying.models;
 
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
 public class Media {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private Long id;
     // private RevengePlan revengePlan;
+    @Enumerated(EnumType.STRING)
+    @NotBlank(message = "Ingrese un tipo de recurso")
     private Type type;
+    @Column(unique = true)
+    @NotBlank(message = "Ingrese una URL")
     private String url;
     private String caption;
 }
